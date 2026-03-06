3391c7a87edbbcd0d5ef3a7fc0fe05c301daf85e
Your Name <ajichukendoclub@gmail.com~>
2026-03-06 12:06:54 +0900

    fix: Add GitHub Actions for Vite deployment and fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // GitHub Pagesなどにデプロイする際、相対パスでアセットを解決させるための設定
-    base: './',
+    // GitHub Pagesなどにデプロイする際、ターゲットリポジトリ名に合わせる
+    base: '/72/',
     server: {
         port: 3000,
         open: false,
